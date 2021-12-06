13019f7b3e20a23f9ea2550f69086b0a3deb6e32
adi26692@gmail.com <adi26692@gmail.com>
2021-12-06 17:57:07 +0530

    details page

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,60 @@
 $(document).ready(function () {
-  $(".owl-carousel").owlCarousel();
-  console.log("dwwdwdw");
+  var owl = $('.owl-feature');
+  var owlStory = $('.owl-story');
+  owl.owlCarousel({
+    loop: true,
+    nav: true,
+    margin: 10,
+    autoplay: true,
+    pagination: true,
+    paginationNumbers: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 3
+      },
+      960: {
+        items: 4
+      },
+      1200: {
+        items: 4
+      }
+    }
+  });
+
+  owlStory.owlCarousel({
+    loop: true,
+    nav: true,
+    margin: 10,
+    autoplay: true,
+    pagination: true,
+    paginationNumbers: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 1
+      },
+      1200: {
+        items: 2
+      }
+    }
+  });
+
 });
 
-$(document).ready(function () {
-  console.log("ready!");
+
+$(document).on('scroll', function () { 
+  if ($(window).scrollTop() > 700) {
+    $('.navbar').addClass('change-color');
+  } else {
+    $('.navbar').removeClass('change-color');
+  }
 });
